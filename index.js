e48bb54ccb03aff392965ee7d9294f7f39ce2b35
userssb <sreeramoju.sreekanthbabu@gmail.com>
2022-04-06 13:24:20 +0000

    commit React JS First example

---
+++ index.js
@@ -0,0 +1,17 @@
+const element = (
+  <div class="bgcont">
+    <h1 class="heading">Super Over League</h1>
+    <diV class="img-cont">
+      <img
+        src="https://assets.ccbp.in/frontend/react-js/rcb-img.png"
+        class="image"
+      />
+      <img
+        src="https://assets.ccbp.in/frontend/react-js/csk-img.png"
+        class="image"
+      />
+    </diV>
+  </div>
+);
+
+ReactDOM.render(element, document.getElementById("root"));
